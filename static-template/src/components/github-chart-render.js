2df5824a1a0bf76b4a731484accd4c1fa669a533
Deep <shecallme3000@gmail.com>
2025-12-05 21:33:46 +0530

    feat: Add grid to gitchart svg

--- static-template/src/components/github-chart-render.js
+++ static-template/src/components/github-chart-render.js
@@ -1,20 +1,23 @@
 const createConfig = () => {
+  // main svg frame
   const frame = {
     x: 0,
     y: 0,
     width: 738,
     height: 156,
   };
-  frame.viewport = `0 0 ${frame.width} ${frame.height}`;
+  frame.viewbox = `0 0 ${frame.width} ${frame.height}`;
 
+  // graph header
   const header = {
     x: 0,
     y: 0,
     width: frame.width,
     height: 24,
   };
-  header.viewport = `0 0 ${header.width} ${header.height}`;
+  header.viewbox = `0 0 ${header.width} ${header.height}`;
 
+  // graph grid
   const grid = {
     x: 0,
     y: 28,
@@ -22,10 +25,12 @@ const createConfig = () => {
     height: 94,
     cellSize: 10,
     cellGap: 4,
+    cellRx: 2,
     totalCols: 53,
   };
-  grid.viewport = `0 0 ${grid.width} ${grid.height}`;
+  grid.viewbox = `0 0 ${grid.width} ${grid.height}`;
 
+  // graph foot
   const footer = {
     x: 0,
     y: 132,
@@ -41,8 +46,8 @@ const createConfig = () => {
       right: { x: 111, y: 7.5 },
     },
   };
-  footer.viewport = `0 0 ${footer.width} ${footer.height}`;
-  footer.meta.viewport = `0 0 ${footer.meta.width} ${footer.meta.height}`;
+  footer.viewbox = `0 0 ${footer.width} ${footer.height}`;
+  footer.meta.viewbox = `0 0 ${footer.meta.width} ${footer.meta.height}`;
 
   return { frame, header, grid, footer };
 };
@@ -52,21 +57,68 @@ export default class GitChartRenderer {
   constructor(container, config) {
     this.container = container;
     this.config = { ...CONFIG, ...config };
-    this.svgElement = null;
+    this.svgElem = null;
   }
 
   renderSkeleton() {
-    this._createFrame();
+    // create components
+    this.svgElem = this._createFrame();
+    this.gridElem = this._createGrid();
+
+    // append components
+    this.svgElem.appendChild(this.gridElem);
+
+    // append svg
+    this.container.appendChild(this.svgElem);
   }
 
-  _createElem(type, attrs) {
+  _createElem(type, attrs, preserveAspectRatio = "none") {
     const svgNs = "http://www.w3.org/2000/svg";
     const elem = document.createElementNS(svgNs, type);
-    Object.entries(attrs).forEach((key, value) =>
-      elem.setAttribute(key, value)
-    );
+    Object.entries(attrs).forEach(([key, value]) => {
+      elem.setAttribute(key, value);
+    });
+    elem.setAttribute("preserveAspectRatio", preserveAspectRatio);
     return elem;
   }
 
-  _createFrame() {}
+  _createFrame() {
+    const config = this.config.frame;
+    const elem = this._createElem("svg", {
+      x: config.x,
+      y: config.y,
+      width: config.width,
+      height: config.height,
+      viewbox: config.viewbox,
+    });
+    return elem;
+  }
+
+  _createGrid() {
+    const config = this.config.grid;
+    const elem = this._createElem("svg", {
+      x: config.x,
+      y: config.y,
+      width: config.width,
+      height: config.height,
+      viewbox: config.viewbox,
+    });
+
+    // create boxes
+    for (let col = 0; col < config.totalCols; col++) {
+      for (let row = 0; row < 7; row++) {
+        const cellElem = this._createElem("rect", {
+          x: col * (config.cellSize + config.cellGap),
+          y: row * (config.cellSize + config.cellGap),
+          width: config.cellSize,
+          height: config.cellSize,
+          rx: config.cellRx,
+          "data-level": 0,
+        });
+        elem.appendChild(cellElem);
+      }
+    }
+
+    return elem;
+  }
 }
